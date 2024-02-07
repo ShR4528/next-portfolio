fd1d779f9cccf4b7939a648cacc97c3cb3270c24
Shr4528 <Shr4528@gmail.com>
2024-02-07 12:36:44 -0600

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='h-full flex items-center justify-between'>
+      <div>1</div>
+    </div>
+  );
+};
+
+export default Navbar;
