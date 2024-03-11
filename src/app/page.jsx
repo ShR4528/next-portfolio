bbb493383ab5df165cf0e8b97907d959ab6376d8
Shr4528 <Shr4528@gmail.com>
2024-03-11 14:05:10 -0500

    transition

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,14 +8,15 @@ const Homepage = () => {
     <motion.div
       className='h-full'
       initial={{ y: '-200vh' }}
-      animate={{ y: '0vh%' }}
+      animate={{ y: '0%' }}
       transition={{ duration: 1 }}>
-      <div className='h-full flex flex-col  lg:flex-row px-4 sm:px-8 md:px-12px lg:px-20 xl:px-48'>
-        {/* image container */}
+      <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+        {/* IMAGE CONTAINER */}
         <div className='h-1/2 lg:h-full lg:w-1/2 relative'>
-          <Image src='/hero.png' alt='hero' className='object-contain' fill />
+          <Image src='/hero.png' alt='' fill className='object-contain' />
         </div>
         {/* text container */}
+
         <div className='h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center'>
           {/* title */}
           <h1 className='text-4xl md:text-6xl font-bold'>
