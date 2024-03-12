7a4b94325a08fa919a9b2652a1ca0f32beb6aa8c
Shr4528 <Shr4528@gmail.com>
2024-03-12 12:33:34 -0500

    myworks

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,15 +1,24 @@
 'use client';
 import { motion, useScroll, useTransform } from 'framer-motion';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const PortfolioPage = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll(ref);
+
+  const x = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+
   return (
     <motion.div
       className='h-full'
       initial={{ y: '-200vh' }}
-      animate={{ y: '0    %' }}
+      animate={{ y: '0%' }}
       transition={{ duration: 1 }}>
-      <div className='h-[600vh] relative'> porsfs</div>
+      <div className='h-[600vh]' ref={ref}>
+        <div className='w-screen h-[calc(100vh-6rem)] flex items-center justify-center  text-8xl text-center'>
+          My Works
+        </div>
+      </div>
     </motion.div>
   );
 };
