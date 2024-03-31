f7fd31a208caf9ded3b340388c4366d78d53244f
Shr4528 <Shr4528@gmail.com>
2024-03-31 15:38:19 -0500

    svg update

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -64,7 +64,7 @@ const PortfolioPage = () => {
             <div className="flex flex-col gap-8 text-white">
               <h1 className=''>{item.title}</h1>
               <div className='relative'>
-                <Image src={item.img} alt='' fill />
+                <Image src={item.img} alt='ph' fill />
               </div>
               <p className='text-center'>{item.desc}</p>
               <Link href={item.link}>
@@ -74,21 +74,37 @@ const PortfolioPage = () => {
           </div>
           ))}
         </motion.div>
+        </div>
+      </div>
         <div className="w-screen flex flex-col gap-16 items-center justify-center text-center">
-          <h1 className='text-4xl'>Do you have project</h1>
+          <h1 className='text-8xl'>Do you have a project?</h1>
           <div className='relative'>
-            <svg>
+            <motion.svg
+               animate={{ rotate: 360 }}
+               transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            className="w-64 h-64 md:w-[500px] md:h-[500px]"
+              viewBox="0 0 300 300"
+            >
             <defs>
               <path
                 id="circlePath"
                 d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
               />
             </defs>
-            </svg>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Front-end Developer and UI Designer
+              </textPath>
+            </text>
+            </motion.svg>
+            <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+          >
+            Hire Me
+          </Link>
           </div>
         </div>
-        </div>
-      </div>
     </motion.div>
   );
 };
